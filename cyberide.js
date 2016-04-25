488dda480e9741dcf9b8a78cba35cf964d875296
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-04-25 01:56:55 -0400

    Animation

--- cyberide.js
+++ cyberide.js
@@ -1,7 +1,6 @@
 
 var Cyberide = function() {
-	this.posts = [];
-	this.images = [];
+	this.content = [];
 };
 
 /** STATIC PROPERTIES */
@@ -18,10 +17,8 @@ Cyberide.prototype.onStatusChange = function(response) {
 	if (response.status === 'connected') {
 		console.log('Connected');
 		this.loadContent();
-	} else if (response.status === 'not_authorized') {
-		FB.login(this.onLogin, {scope: 'user_posts, user_photos'});
 	} else {
-		console.error('Not logged in');
+		FB.login(this.onLogin, {scope: 'user_posts, user_photos'});
 	}
 };
 
@@ -35,21 +32,45 @@ Cyberide.prototype.onLogin = function(response) {
 };
 
 Cyberide.prototype.loadContent = function() {
-	FB.api('/me/photos', 'get', {fields: 'source'}, function(response) {
-		for (var i = 0; i < response.data.length; i++) {
-			this.images.push($('<img class="fb-image fb-content" src="' + response.data[i].source +'">'));
-			$('#painting').append(this.images[i]);
-		}
+	FB.api('/me', 'get', {}, function(response) {
+		$('#name')
+			.text(response.name)
+			.removeClass('hidden');
 	}.bind(this));
 
-	FB.api('/me/posts', 'get', {}, function(response) {
+	FB.api('/me/photos', 'get', {fields: 'source', limit: '500'}, function(response) {
 		for (var i = 0; i < response.data.length; i++) {
-			if (!response.data[i].message) continue;
-			this.posts.push($('<div class="fb-post fb-content">' + response.data[i].message + '</div>'));
-			$('#painting').append(this.posts[i]);
+			this.content.push($('<img class="fb-image fb-content hidden" src="' + response.data[i].source +'">'));
+			$('#painting').append(this.content[i]);
 		}
-		console.log(this.posts);
+
+		FB.api('/me/posts', 'get', {limit: '500'}, function(response) {
+			for (var i = 0; i < response.data.length; i++) {
+				if (!response.data[i].message) continue;
+				this.content.push($('<div class="fb-post fb-content hidden">' + response.data[i].message + '</div>'));
+				$('#painting').append(this.content[i]);
+			}
+
+			this.animateContent();
+		}.bind(this));
 	}.bind(this));
+};
+
+Cyberide.prototype.animateContent = function() {
+	for (var i = 0; i < this.content.length; i++) {
+		var order = Math.random();
+
+		this.content[i]
+			.offset({
+				left: Math.floor(Math.random() * 600) + window.innerWidth/2 - 300,
+				top: Math.floor(Math.random() * (window.innerHeight-350)) + 240
+			})
+			.css({
+				'transition-duration': (order * 100 + 5) + 's',
+				'z-index': Math.floor(order * 1000)
+			})
+			.removeClass('hidden');
+	}
 };
 
 /** INITIALIZATION */
